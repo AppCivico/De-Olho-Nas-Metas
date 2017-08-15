f9105021ddc4d71038ddce951cfbf4bfdd4f59b6
Junior Moraes <juniorfvox@gmail.com>
2017-08-15 11:44:05 -0300

    Instalando dependencias das dists no docker.

--- build-container.sh
+++ build-container.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-cp Makefile.PL docker/Makefile_local.PL
+cp api/Makefile.PL docker/Makefile_api.PL
+cp web/Makefile.PL docker/Makefile_web.PL
+
 docker build -t appcivico/donm docker/
+
+rm docker/Makefile_api.PL docker/Makefile_web.PL
